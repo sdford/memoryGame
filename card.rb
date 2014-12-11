f0b31305ac9c57340b3e7ae5b707b75981469e57
Shaun Ford <shaun.deandre@yahoo.com>
2014-12-11 14:29:10 -0500

    initial commit; infrastructure for the memory game is setup; about to start testing

---
+++ card.rb
@@ -0,0 +1,36 @@
+class Card
+  SYMBOLS = ['AH','2D','JC','QS','8D','4H']
+  FLIPPED_DOWN_STATE = 0
+  FLIPPED_UP_STATE = 1
+  DEFAULT_STATE = FLIPPED_DOWN_STATE
+
+  def self.random_card
+    random_symbol = SYMBOLS[ rand(0..SYMBOLS.size) ]
+    default_state = DEFAULT_STATE
+    Card.new(random_symbol,default_state)
+  end
+
+  def flip!
+    if !self.state.nil?
+      current_state = self.state
+      @state = ((current_state == FLIPPED_UP_STATE) ? FLIPPED_DOWN_STATE : FLIPPED_UP_STATE)
+    end
+  end
+
+  private
+  def initialize(symbol, state)
+    random_symbol = SYMBOLS[ rand(0..SYMBOLS.size) ]
+    @symbol = random_symbol
+    @state = DEFAULT_STATE
+  end
+
+  # def check_symbol
+  #   if self.symbol.nil?
+  #     false
+  #   else
+  #     SYMBOLS.include?(self.symbol)
+  #   end
+  # end
+
+  
+end
---
+++ memory_game.rb
@@ -0,0 +1,132 @@
+class MemoryGame
+  #make sure num of cards is even
+  #even num of cols => even num of cards
+  @num_rows = 2
+  @num_cols = 3
+
+  #make sure num_cards/2 < Card::Symbols.size
+  @num_cards = @@num_rows * @@num_cols
+  
+  @num_possible_matches = @num_cards/
+  @num_found_matches = 0
+
+  @first_card = nil
+  @second_card = nil
+
+  def initialize
+    @cards = []
+    @board = []
+
+    uniq_cards = []
+    while uniq_cards.size < (@num_cards / 2)
+      card = Card.random_card
+      if is_new_card?(card,uniq_cards)
+        uniq_cards.append(card)
+      end
+    end
+    make_board(uniq_cards)
+    start_turn
+  end
+
+  def start_turn
+    print_board
+
+    puts "Please enter a row (1-#{@board.size})"
+    row = gets.chomp
+
+    if (1..@board.size+1).include?(row) 
+      row -= 1 
+      puts "Please enter a col (1-#{@board[row].size}"
+      col = gets.chomp
+
+      if (1..@board[row].size+1).include?(col) 
+        col -= 1
+        selected_card = @board[row][col]
+
+        #if first turn
+        if @first_card.nil?
+          selected_card.flip!
+          @first_card = clicked_card
+
+        #if second turn
+        elsif @second_card.nil?
+          if selected_card.state == Card::FLIPPED_UP_STATE
+            puts "Card already flipped up."
+            print_board
+            return false
+          else
+            selected_card.flip!
+            check_similarity
+          end
+        end
+        print_board
+        return true
+
+      else
+        puts "Invalid col."
+        return false
+      end
+    else
+      puts "Invalid row."
+      return false
+    end
+
+  end
+
+  def print_board
+    print '['
+    for row in @board
+      print '['
+      for card in row
+        if card.state == Card::FLIPPED_UP_STATE
+          print card.symbol
+        else
+          print 'X'
+        end
+        print ', '
+      end
+      print ']'
+      print "\n"
+    end
+    print ']'
+    print "\n"
+  end
+
+
+  private
+
+  def is_new_card?(card,card_arr)
+    card_arr.map{|c| c.symbol}.include?(card.symbol)
+  end
+
+  def make_board(uniq_cards)
+    @cards = uniq_cards + uniq_cards
+    @cards.shuffle!
+
+    card_index = 0
+    for row_index in (0..@num_rows)
+      row = []
+      for col_index in (0..@num_cols)
+        row.append(@cards[card_index])
+        card_index += 1
+      end
+      @board.append(row)
+    end
+  end
+
+  def check_similarity
+    if !@first_card.nil? && !@second_card
+      if @first_card.symbol.equals?(@second_card.symbol)
+        @first_card = nil
+        @second_card = nil
+      else
+        sleep(3)
+        @first_card.flip!
+        @second_card.flip!
+      end
+    end
+  end
+end
+
+game = MemoryGame.new
+game.start_turn
